cce28e6a1839196a0a2e012265a9cf1236c63d9c
amb231143 <amb231143@uvg.edu.gt>
2024-08-05 08:57:04 -0600

    Botón y modificaciones
    Se creo la interacción del botón y se modificaron los textos para que se vean más organizados.

--- app/src/main/java/com/uvg/lab1_pm/MainActivity.kt
+++ app/src/main/java/com/uvg/lab1_pm/MainActivity.kt
@@ -9,9 +9,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -27,12 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab1PMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        message1 = "Dulce Ambrosio",
-                        message2 = "Plataformas Móviles",
-                        message3 = "Laboratorio 1",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -40,20 +41,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(message1: String, message2: String, message3: String, modifier: Modifier = Modifier) {
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greeting(message2 = "Plataformas Móviles", message3 = "Laboratorio 1")
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Dulce Ambrosio")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continuar")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun OnboardingScreenPreview() {
+    Lab1PMTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+fun Greeting(message2: String, message3: String, modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxSize().padding(8.dp)
-    ){
-        Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Column(
             verticalArrangement = Arrangement.Center,
-            modifier = modifier.padding(8.dp)){
-            Text(
-                text = message1,
-                fontSize = 30.sp,
-                lineHeight = 50.sp,
-                textAlign = TextAlign.Center
-            )
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.padding(8.dp)
+        ) {
             Text(
                 text = message2,
                 fontSize = 20.sp,
@@ -66,14 +106,20 @@ fun Greeting(message1: String, message2: String, message3: String, modifier: Mod
             )
         }
     }
-
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     Lab1PMTheme {
-        Greeting("Dulce Ambrosio", message2 = "Plataformas Móviles", message3 = "Laboratorio 1")
+        Greeting(message2 = "Plataformas Móviles", message3 = "Laboratorio 1")
+    }
+}
 
+@Preview
+@Composable
+fun MyAppPreview() {
+    Lab1PMTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
