0efc951d06eba20ead75eadbb9924ee657cdadb0
amb231143 <amb231143@uvg.edu.gt>
2024-08-04 22:30:16 -0600

    Textos
    Se colocaron 3 textos diferentes en el centro de la aplicación, sin que se traslapen.

--- app/src/main/java/com/uvg/lab1_pm/MainActivity.kt
+++ app/src/main/java/com/uvg/lab1_pm/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uvg.lab1_pm.ui.theme.Lab1PMTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +28,9 @@ class MainActivity : ComponentActivity() {
             Lab1PMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        message1 = "Dulce Ambrosio",
+                        message2 = "Plataformas Móviles",
+                        message3 = "Laboratorio 1",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(message1: String, message2: String, message3: String, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize().padding(8.dp)
+    ){
+        Column (
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(8.dp)){
+            Text(
+                text = message1,
+                fontSize = 30.sp,
+                lineHeight = 50.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = message2,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = message3,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab1PMTheme {
-        Greeting("Android")
+        Greeting("Dulce Ambrosio", message2 = "Plataformas Móviles", message3 = "Laboratorio 1")
+
     }
 }
